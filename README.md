ff0284750e79052a5c82702b75e3757ec5a080fa
Muhammad Yasir Ghaffar <mghaffar.bese21seecs@seecs.edu.pk>
2023-08-15 06:10:30 +0500

    2nd commit added files

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
-"# Real-Estate" 
+# Real Estate Website Showcase
+
+This is a responsive real estate website showcasing my skills in HTML, CSS, and JavaScript. The website aims to demonstrate a visually appealing and functional design for a real estate agency. The website features various sections such as home, popular properties, values, contact, and footer.
+
+## Features
+
+- Responsive design to ensure optimal user experience across different devices and screen sizes.
+- Interactive navigation menu with smooth scrolling to different sections of the page.
+- Popular properties section using a swiper carousel for showcasing featured properties.
+- Values section with accordion-style content for presenting key values and information.
+- Contact section with contact information and a contact card layout.
+- Dark mode feature for better readability in low-light environments.
+- Scroll-to-top button for easy navigation back to the top of the page.
+- Theme toggling to switch between light and dark modes.
+  
+## Technologies Used
+
+- HTML: Structured the website content and layout.
+- CSS: Styled the website components, sections, and responsive design.
+- JavaScript: Implemented interactive features such as smooth scrolling, theme toggling, accordion behavior, and swiper carousel.
+
+## Getting Started
+
+1. Clone this repository to your local machine using:
+   ```
+   git clone https://github.com/your-username/real-estate-website.git
+   ```
+
+2. Open the `index.html` file in your web browser to view the website.
+
+## Usage
+
+- Navigate through the website using the interactive menu and smooth scrolling.
+- Explore the popular properties section to view featured properties.
+- Expand and collapse the values accordion to learn more about the company's values.
+- Contact the agency using the provided contact information.
+
+## Contributing
+
+Contributions are welcome! If you find any issues or have suggestions for improvements, please open an issue or create a pull request.
---
+++ assets/js/main.js
@@ -0,0 +1,125 @@
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+function scrollHeader() {
+    const header = document.getElementById('header')
+    if (this.scrollY >= 50) {
+        header.classList.add('scroll-header')
+    }
+}
+window.addEventListener('scroll', scrollHeader)
+
+/*=============== SWIPER POPULAR ===============*/
+var swiper = new Swiper(".popular__container", {
+
+    spaceBetween: 32,
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPreview: 'auto',
+    loop: true,
+
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+
+/*=============== VALUE ACCORDION ===============*/
+const toggleItem = (item) => {
+    const accordionContent = item.querySelector('.value__accordion-content')
+
+    if (item.classList.contains('accordion-open')) {
+        accordionContent.removeAttribute('style')
+        item.classList.remove('accordion-open')
+    } else {
+        accordionContent.style.height = accordionContent.scrollHeight + 'px'
+        item.classList.add('accordion-open');
+    }
+}
+
+const accordionItems = document.querySelectorAll('.value__accordion-item')
+accordionItems.forEach((item) => {
+    const accordionHeader = item.querySelector('.value__accordion-header');
+    accordionHeader.addEventListener('click', () => {
+        const openItem = document.querySelector('.accordion-open')
+        toggleItem(item)
+
+        if (openItem && openItem !==item) {
+            toggleItem(openItem);
+        }
+    });
+});
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]');
+
+function scrollActive() {
+    const scrollY = window.pageYOffset; // Use pageYOffset instead of pageXOffset
+
+    sections.forEach((current) => {
+        const sectionHeight = current.offsetHeight,
+              sectionTop = current.offsetTop - 58,
+              sectionId = current.getAttribute('id');
+
+        // Fix the typo in the variable name 'scroollY'
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link');
+        } else {
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link'); // Change 'add' to 'remove'
+        }
+    });
+}
+
+window.addEventListener('scroll', scrollActive);
+
+/*=============== SHOW SCROLL UP ===============*/
+function scrollUP() {
+    const scrollUp = document.getElementById('scroll-up');
+
+    if (window.scrollY >= 350) {
+        scrollUp.classList.add('show-scroll');
+    } else {
+        scrollUp.classList.remove('show-scroll');
+    }
+}
+
+window.addEventListener('scroll', scrollUP);
+
+// Attach click event listener to the scroll-up button
+const scrollUpButton = document.getElementById('scroll-up');
+scrollUpButton.addEventListener('click', (event) => {
+    event.preventDefault(); // Prevent default anchor tag behavior
+
+    // Smoothly scroll to the top of the page (home section)
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+});
+
+
+/*=============== DARK LIGHT THEME ===============*/
+
+
+const themeButton = document.getElementById('theme-button');
+const darkTheme = 'dark-theme';
+const iconTheme = 'bx-sun'; // Corrected icon class
+
+// Function to toggle the theme
+const toggleTheme = () => {
+    document.body.classList.toggle(darkTheme);
+    themeButton.classList.toggle(iconTheme);
+
+    // Save the user's theme preference in local storage
+    localStorage.setItem('selected-theme', document.body.classList.contains(darkTheme) ? 'dark' : 'light');
+    localStorage.setItem('selected-icon', themeButton.classList.contains(iconTheme) ? 'bx bx-moon' : 'bx bx-sun');
+};
+
+// Check user's preferred theme from local storage
+const selectedTheme = localStorage.getItem('selected-theme');
+const selectedIcon = localStorage.getItem('selected-icon');
+
+if (selectedTheme) {
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme);
+    themeButton.classList[selectedIcon === 'bx bx-moon' ? 'add' : 'remove'](iconTheme);
+}
+
+themeButton.addEventListener('click', toggleTheme);
